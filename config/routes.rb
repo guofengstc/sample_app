7712488509cd54113df9d731a5adc877d7641647
tom <guofengstc@gmail.com>
2012-11-23 15:09:10 +0800

    add rspec

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ SampleApp::Application.routes.draw do
 
   get "static_pages/about"
 
+  get "static_pages/contact"
+
   get 'home' => "static_pages#home"
 
 
@@ -67,6 +69,6 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
    
-  # match ':controller(/:action(/:id))(.:format)'
+   match ':controller(/:action(/:id))(.:format)'
 
 end
