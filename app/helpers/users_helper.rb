ebcc405212a80f63cec5831791359751ee3aff2c
tom <guofengstc@gmail.com>
2012-12-03 05:01:41 +0800

    Finish user edit, update, index, and destroy actions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,6 +8,7 @@ module UsersHelper
   def gravatar_for(user, options={size:50})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options.size}"
+    # puts "gravatar_url=" << gravatar_url
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
